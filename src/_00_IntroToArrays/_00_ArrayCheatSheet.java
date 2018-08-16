b5c8a90569071a79f26a69af6512b07a3baeec81
aleksaradomirovic <34846410+aleksaradomirovic@users.noreply.github.com>
2018-08-15 18:32:23 -0700

    Add files via upload

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -5,29 +5,51 @@ import java.util.Random;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
+		String[] array = new String[5];
 		//2. print the third element in the array
-
+		System.out.println(array[2]);
 		//3. set the third element to a different value
-
+		array[2] = "potato";
 		//4. print the third element again
-		
+		System.out.println(array[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for(int i = 0; i < array.length; i++) {
+			array[i] = "potato";
+		}
 		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 		
 		//7. make an array of 50 integers
-
+		int[] intarray = new int[50];
 		//8. use a for loop to make every value of the integer array a random number
-
+		Random rnd = new Random();
+		for(int i = 0; i < intarray.length; i++) {
+			intarray[i] = rnd.nextInt();
+		}
 		//9. without printing the entire array, print only the smallest number on the array
-
+		int smallestIndex = 0;
+		for(int i = 0; i < intarray.length; i++) {
+			if(intarray[i] < intarray[smallestIndex]) {
+				smallestIndex = i;
+			}
+		}
+		System.out.println(intarray[smallestIndex] + "\n");
+		
 		//10 print the entire array to see if step 8 was correct
-
+		for(int i = 0; i < intarray.length; i++) {
+			System.out.println(intarray[i]);
+		}
 		//11. print the largest number in the array.
-		
+		int largestIndex = 0;
+		for(int i = 0; i < intarray.length; i++) {
+			if(intarray[i] > intarray[largestIndex]) {
+				largestIndex = i;
+			}
+		}
+		System.out.println("\n"+intarray[largestIndex]+"\n");
 		//12. print only the last element in the array
-		
+		System.out.println(intarray[intarray.length-1]);
 	}
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -3,28 +3,54 @@ package _00_IntroToArrays;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.GapContent;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
+		boolean raceFinished = false;
+		Random rnd = new Random();
 		//2. create an array of 5 robots.
-
-		//3. use a for loop to initialize the robots.
-
+		Robot[] robotArmy = new Robot[5];
+				//3. use a for loop to initialize the robots.
+		for(int i = 0; i < robotArmy.length; i++) {
+			robotArmy[i] = new Robot();
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-		//7. declare that robot the winner and throw it a party!
-    	
-		//8. try different races with different amounts of robots.
-    	
-	    //9. make the robots race around a circular track.
-
-	
+			robotArmy[i].setX(100+i*100);
+			robotArmy[i].setY(500);
+			robotArmy[i].setAngle(0);
+			robotArmy[i].setSpeed(10);
+		}
+					
+			
+				//5. use another for loop to iterate through the array and make each robot move 
+			    //   a random amount less than 50.
+		    	
+				//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		while(!raceFinished) {
+			for(int i = 0; i < robotArmy.length; i++) {
+				robotArmy[i].move(rnd.nextInt(50));
+			}
+			for(int i = 0; i < robotArmy.length; i++) {
+				if(robotArmy[i].getY() < 25) {
+					raceFinished = true;
+				}
+			}
+		}
+		int winner = 0;
+		for(int i = 0; i < robotArmy.length; i++) {
+			if(robotArmy[i].getY() < robotArmy[winner].getY()) {
+				winner = i;
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, "RACE FINISHED\n 1st place: Robot "+winner+"!");
+		    	
+				//8. try different races with different amounts of robots.
+		    	
+			    //9. make the robots race around a circular track.
+		//told to skip 8 & 9
+	}
 }
